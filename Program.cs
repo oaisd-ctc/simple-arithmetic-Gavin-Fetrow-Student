c230bcaddb750bc52a4bb0fa8d5d891ded095d8f
Gavin-Fetrow-Student <gafe134@student.oaisd.org>
2024-10-17 14:24:26 +0000

    Use a basic arithmetic

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Provide a number to perform the addition operation.");
+        string number = Console.ReadLine();
+        Console.WriteLine(number + "... alright. Provide a number to add to " + number + ".");
+        string secondNumber = Console.ReadLine();
+        Console.WriteLine(secondNumber + ", ok then! Pretty simple. " + number + " + " + secondNumber + " is " + number + secondNumber);
+    }
+}
